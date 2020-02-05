2bd6cc118ea7a7a11e973b0b3357287a85ddf2d9
aibotsoft <aibotsoft@gmail.com>
2020-02-05 14:05:46 +0200

    add fastApi

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main() -> str:
-    return 'hello world'
+from fastapi import FastAPI, Path, Query
+
+app = FastAPI()
+
+
+@app.api_route("/api_route")
+def non_operation():
+    return {"message": "Hello World"}
+
+
+def non_decorated_route():
+    return {"message": "Hello World"}
+
+
+app.add_api_route("/non_decorated_route", non_decorated_route)
 
 
 if __name__ == '__main__':
-    print(main())
+    print('hello')
---
+++ tests/test_application.py
@@ -0,0 +1,20 @@
+import pytest
+from starlette.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+@pytest.mark.parametrize(
+    "path,expected_status,expected_response",
+    [
+        ("/api_route", 200, {"message": "Hello World"}),
+        ("/non_decorated_route", 200, {"message": "Hello World"}),
+        ("/nonexistent", 404, {"detail": "Not Found"}),
+    ],
+)
+def test_get_path(path, expected_status, expected_response):
+    response = client.get(path)
+    assert response.status_code == expected_status
+    assert response.json() == expected_response
